09a1e30bd5790a36fe5159b3867e5f7df6d92849
Alexandre Muller Junior <alexandremj@pm.me>
2020-03-27 10:39:39 -0300

    Add validation with celebrate

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate'); 
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(cors());
 // permite corpo de requisições enviados como JSON
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 /*
  * Tipos de parâmetros
